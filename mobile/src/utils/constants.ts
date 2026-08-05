eb7051e3cd1ee5af67dc588deb89ed41904de7b5
Jesse Stover <j.stover.r@gmail.com>
2026-08-04 19:56:14 -0500

    Added constants, removed supabase

---
+++ mobile/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const AUTHENTICATION_SERVICE_ENDPOINT = "http://localhost:5000"
+export const GROUPING_SERVICE_ENDPOINT = "http://localhost:5001"
+export const TIMEDELTA_SERVICE_ENDPOINT = "http://localhost:5002"
